addb2c876ac290fc23ceafb5e681a3621b142b37
cedric <cedric@cedrics-MacBook-Pro.local>
2023-09-26 23:51:46 +0800

    feat: layout

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = "Ced's blog";
+export const description = 'Everything about ced';
+export const url = dev ? 'http://127.0.0.1:5173/' : 'http://cedric.com/';
---
+++ src/routes/header.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import * as config from '$lib/config';
+</script>
+
+<nav>
+	<a href="/" class="title">
+		<b>{config.title}</b>
+	</a>
+
+	<ul class="links">
+		<li>
+			<a href="/about">About</a>
+		</li>
+		<li>
+			<a href="/contact">Contact</a>
+		</li>
+		<li>
+			<a href="/rss.xml" target="_blank">RSS</a>
+		</li>
+	</ul>
+
+	<button>Toggle</button>
+</nav>
